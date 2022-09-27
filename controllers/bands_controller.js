69412840bc09c69164644d92d465ca9749279627
Mason Benzinger <mason.benzinger@gmail.com>
2022-09-26 21:06:43 -0500

    added the association queries

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,8 +1,8 @@
 // DEPENDENCIES
 const bands = require('express').Router()
 const db = require('../models')
-const { Band } = db
 const { Op } = require('sequelize')
+const { Band, MeetGreet, Event, SetTime } = db
 
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
@@ -10,7 +10,7 @@ bands.get('/', async (req, res) => {
         const foundBands = await Band.findAll({
             order: [['available_start_time', 'ASC']],
             where: {
-                name: { [Op.like]:  `%${req.query.name ? req.query.name : ''}%` }
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
             }
         })
         res.status(200).json(foundBands)
@@ -20,16 +20,35 @@ bands.get('/', async (req, res) => {
 })
 
 // FIND A SPECIFIC BAND
-bands.get('/:id', async (req, res) => {
+bands.get('/:name', async (req, res) => {
     try {
         const foundBand = await Band.findOne({
-            where: { band_id: req.params.id }
+            where: { name: req.params.name },
+            include: [
+                {
+                    model: MeetGreet,
+                    as: "meet_greets",
+                    include: { model: Event,
+                    as: "event",
+                    where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+                }
+                },
+                {
+                    model: SetTime,
+                    as: "set_times",
+                    include: { model: Event,
+                    as: "event",
+                    where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+                }
+                }
+            ]
         })
         res.status(200).json(foundBand)
     } catch (error) {
         res.status(500).json(error)
     }
 })
+
 
 // CREATE A BAND
 bands.post('/', async (req, res) => {
